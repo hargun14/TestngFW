89dc4fcac6eefa54f4db72340a89a248285d5d06
Hargun14 <hargunkaur20@gmail.com>
2020-05-06 23:43:50 +0530

    feat: adding property util

---
+++ src/main/java/utils/PropertyUtil.java
@@ -0,0 +1,51 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author DELL
+ *
+ */
+public class PropertyUtil {
+
+	private static final Map<String, Properties> CACHE = new ConcurrentHashMap<String, Properties>();
+	
+	public static String get(String fileName, String key)
+	{
+		return loadProperties(fileName).getProperty(key);
+	}
+
+
+	private static Properties loadProperties(String fileName) {
+		// TODO Auto-generated method stub
+		if(CACHE.containsKey(fileName)) {
+			return CACHE.get(fileName);
+		}
+		else {
+			InputStream inputStream = PropertyUtil.class.getClassLoader().getResourceAsStream(fileName);
+			if(inputStream != null) {
+				Properties property = new Properties();
+				try {
+					property.load(inputStream);
+					return property;
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					System.out.println("Unable toload file");
+				}
+			}
+			else {
+				System.out.println("file not found");
+			}
+				
+		}
+		return new Properties();
+	}
+
+}
